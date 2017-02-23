ef88ec2265edf4c701c74143e4d02714cf0a46ee
shaun lloyd <shaunlloydmedia@gmail.com>
2017-02-23 14:27:21 -0500

    Add a toggle button component

---
+++ src/app/video-games/detail/components/button-toggle/button-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button-toggle',
+  templateUrl: './button-toggle.component.html',
+  styleUrls: ['./button-toggle.component.scss']
+})
+export class ButtonToggleComponent {
+
+  @Input()
+  public active: boolean;
+
+  @Output()
+  public onToggle = new EventEmitter();
+}
